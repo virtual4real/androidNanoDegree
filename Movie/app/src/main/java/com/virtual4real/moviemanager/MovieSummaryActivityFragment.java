8613602deef64026f1b095c5a6bf0d814e5b2ad3
ioanagosman <ioana.gosman@virtual4real.com>
2015-09-22 00:44:44 +0200

    initial commit

---
+++ Movie/app/src/main/java/com/virtual4real/moviemanager/MovieSummaryActivityFragment.java
@@ -0,0 +1,22 @@
+package com.virtual4real.moviemanager;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MovieSummaryActivityFragment extends Fragment {
+
+    public MovieSummaryActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_movie_summary, container, false);
+    }
+}
