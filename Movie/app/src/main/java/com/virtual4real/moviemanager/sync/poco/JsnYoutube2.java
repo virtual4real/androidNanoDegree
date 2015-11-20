befb38a7703242cbfe1b1d534150bb407b80e775
ioanagosman <ioana.gosman@virtual4real.com>
2015-11-20 16:06:16 +0100

    trailers and reviews

---
+++ Movie/app/src/main/java/com/virtual4real/moviemanager/sync/poco/JsnYoutube2.java
@@ -0,0 +1,51 @@
+package com.virtual4real.moviemanager.sync.poco;
+
+/**
+ * Created by ioanagosman on 13/11/15.
+ */
+public class JsnYoutube {
+    private String source;
+
+    private String name;
+
+    private String type;
+
+    private String size;
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassPojo [source = " + source + ", name = " + name + ", type = " + type + ", size = " + size + "]";
+    }
+}
